a69cfc81c00c136c9b81e6ff41744df8ba2001cf
Caleb Paul and Sara Jensen <sara@>
2016-12-01 10:11:28 -0800

    add logic to log actor name in logcat when input in the main activity upon button click.

--- app/src/main/java/com/epicodus/movieapp/MainActivity.java
+++ app/src/main/java/com/epicodus/movieapp/MainActivity.java
@@ -1,13 +1,37 @@
 package com.epicodus.movieapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.titleTextView) TextView mTitleTextView;
+    @Bind(R.id.searchActorEditTextView) EditText mSearchActorEditTextView;
+    @Bind(R.id.searchActorButton) Button mSearchActorButton;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSearchActorButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSearchActorButton) {
+            String actor = mSearchActorEditTextView.getText().toString();
+            Log.d(TAG, actor);
+        }
+    }
+
 }
